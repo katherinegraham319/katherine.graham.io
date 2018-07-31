a64cb5603f8b4de31a7b9f881c8e958fafa11603
katherinegraham319 <katherinegraham319@gmail.com>
2018-07-31 14:02:50 -0400

    firstproject

---
+++ myscript.js
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<head>
+function buttonOnClick(){
+  document.getElementById('info').innerHTML= "<a href="https://www.youtube.com/watch?v=VwwiO9tZHs0"
+}
+</head>
+<body>
+
+</body>
